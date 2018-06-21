be1e03325e0da7a179d39fce261f03073cc9a0ae
Wolfgang Schreyer <w.schreyer@tum.de>
2018-06-21 03:34:43 -0400

    Accidental beam loss, BL1U position 2

--- compile.sh
+++ compile.sh
@@ -3,6 +3,8 @@
 module load gcc/7.3.0
 export FLUPRO=$HOME/fluka
 export FLUFOR=gfortran
+rm -f usimbs.o
 $FLUPRO/flutil/fff usimbs.f
+rm -f myfluka.map myfluka
 $FLUPRO/flutil/lfluka -o myfluka -m fluka usimbs.o
 module load gcc/5.4.0
