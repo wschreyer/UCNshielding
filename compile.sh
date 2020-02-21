9b5969534635f29cad0cfcf3c23d8928bf2e31b8
Wolfgang Schreyer <wschreyer@triumf.ca>
2020-02-21 15:22:07 -0500

    UCN irradiated, larger cryostat pit

--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-module load gcc/7.3.0
+module load gcc/9.1.0
 export FLUPRO=$HOME/fluka
 export FLUFOR=gfortran
 rm -f usimbs.o
 $FLUPRO/flutil/fff usimbs.f
 rm -f myfluka.map myfluka
-$FLUPRO/flutil/lfluka -o myfluka -m fluka usimbs.o
+$FLUPRO/flutil/ldpmqmd -o myfluka usimbs.o
 module load gcc/5.4.0
